d99fc3fe18fff14f40bb41e869094af84c43193d
margoxt <angelicatinio14@gmail.com>
2025-12-24 00:25:21 +0800

    Completed mobile view, and added javascript

---
+++ orders.js
@@ -0,0 +1,39 @@
+const Orders = [
+    {
+        productName: 'Stitch Eat Before you Sleep Vinyl Blindbox',
+        productNumber: '3464',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Stitch Dress Up Show Blindbox',
+        productNumber: '4560',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined', 
+    },
+    {
+        productName: 'Sanrio Baby Latte Vinyl Blindbox',
+        productNumber: '3067',
+        paymentStatus: 'Due',
+        shipping: 'Pending', 
+    },
+    {
+        productName: 'Stitch Hiphop Clothing Blindbox',
+        productNumber: '09567',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered', 
+    },
+    {
+        productName: 'Sanrio Tea Party Vinyl Blindbox',
+        productNumber: '9865',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered', 
+    },
+    {
+        productName: 'Sanrio Tea Party Vinyl Blindbox',
+        productNumber: '67844',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered', 
+    },
+    
+]
